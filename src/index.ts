f329d7231b68ba9a341520df4469ecece7763d36
Alperen Sen <alperensen039@gmail.com>
2026-03-06 23:15:38 +0300

    app.post() fuctions created for messages, users and threads. Thunder Client is used to test the system's functionality.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Hono } from "hono"
 //  means "Look inside my current folder" and  ../ means "go up one level".
 
 import { db } from "../db"
-import { users } from "../db/schema"
+import { users, threads, messages } from "../db/schema"
 
 
 // Create a Hono class object named app. 
@@ -23,11 +23,21 @@ app.get("/",(c)=>{
   return c.text("This is homepage.")
 })
 
-//this is the users section, "async" just before (c) parameter is for allowing function to
-// pause while it waits for the database ------------------------------------------
+//this is the users section------------------------------------------------------
+// "async" just before (c) parameter is for allowing function to
+// pause while it waits for the database 
+//Whenever you ask the database to do something (like read, write, or delete), you must use
+//await to force the JavaScript engine to pause and wait for the physical hard drive to
+//finish its job
 app.get("/users", async(c)=>{
   const allUsers = await db.select().from(users)
   return c.json(allUsers)
+})
+
+app.post("/users", async(c)=>{
+  const body = await c.req.json()
+  await db.insert(users).values({ userName:body.userName })
+  return c.json({message:"User Successfully Created"},201)
 })
 
 
@@ -36,12 +46,30 @@ app.get("/threads", (c)=>{
   return c.text("This is threads page")
 })
 
-
-//this is messages section-------------------------------------------------------------
-app.get("/messages", (c)=>{
-  return c.text("This is messages page")
+app.post("/threads", async(c)=>{
+  const body = await c.req.json()
+  await db.insert(threads).values({threadName:body.threadName})
+  return c.json({message:"Thread Successfully Created"},201)
 })
 
+
+//this is messages section-------------------------------------------------------------
+app.get("/messages", async(c)=>{
+  const allMessages = await db.select().from(messages)
+  return c.json(allMessages)
+})
+
+app.post("/messages", async(c)=>{
+  const body = await c.req.json()
+  const currentTime = new Date().toISOString()
+  await db.insert(messages).values({
+    contentOfMessage: body.contentOfMessage,
+    userID: body.userID,
+    threadID: body.threadID,
+    sendingTime: currentTime
+  })
+  return c.json({message: "Message Successfully Created"},201)
+})
 
 
 
